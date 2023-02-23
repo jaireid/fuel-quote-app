d04f0cb10fd6914d9f9b61ca30f5d8472cb77db2
Jasmaine Williams <jaiwilliamsreid@gmail.com>
2023-02-22 21:16:12 -0600

    Configure vite server and update README

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig( ({ command, mode }) => {
+  process.env.BROWSER = loadEnv(mode, process.cwd(), '').BROWSER
+
+  return {
+      plugins: [react()],
+      server: {
+      open: true,
+    }
+  }
 })
