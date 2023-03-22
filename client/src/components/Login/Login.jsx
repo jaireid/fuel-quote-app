407490dfddb7caacceafe75b3f7aa3905d936635
DMRobbins2000 <dylan.robbins2023@gmail.com>
2023-03-22 12:11:35 -0500

    Login backend (variable assignement)

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,62 +5,83 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-	const [validated, setValidated] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
 
-        if(form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-    	}
+    if(form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-        setValidated(true);
-    };
+    setValidated(true);
+  };
 
-	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	return (
-		<>
-			<h1>Login Form</h1>
-			{/* The noValidate attribute disables the default browser UI for forms */}
-			<Form noValidate validated={validated} onSubmit={handleSubmit}>
-				<Form.Group className='mb-3' controlId='username'>
-          			<Form.Label>Username</Form.Label>
-					<InputGroup hasValidation>
-						<Form.Control
-							type='text' 
-							placeholder='Username' 
-							required
-						/>
-						<Form.Control.Feedback type='invalid'>
-							Username required.
-						</Form.Control.Feedback>
-					</InputGroup>
-        		</Form.Group>
-				<Form.Group className='mb-3' controlId='password'>
-          			<Form.Label>Password</Form.Label>
-					<InputGroup hasValidation>
-						<Form.Control
-							type='password' 
-							placeholder='Password' 
-							required
-						/>
-						<Form.Control.Feedback type='invalid'>
-							Password required.
-						</Form.Control.Feedback>
-					</InputGroup>
-        		</Form.Group>
-				<Button type='submit' variant='outline-dark'>Login</Button>{' '}
-				<Button 
-					variant='dark'
-					onClick={() => {
-						navigate('/register');
-					}}
-				>
-					Register
-				</Button>
-			</Form>
-		</>
-	)
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSave = () => {
+    // Write the data to a file or send it to an API for storage
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className='mb-3' controlId='username'>
+          <Form.Label>Username</Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+              type='text' 
+              placeholder='Username' 
+              required
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <Form.Control.Feedback type='invalid'>
+              Username required.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+              type='password' 
+              placeholder='Password' 
+              required
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <Form.Control.Feedback type='invalid'>
+              Password required.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Button type='submit' variant='outline-dark'>Login</Button>{' '}
+        <Button 
+          variant='dark'
+          onClick={() => {
+            navigate('/register');
+          }}
+        >
+          Register
+        </Button>
+      </Form>
+      <Button variant='success' onClick={handleSave}>
+        Save
+      </Button>
+    </>
+  );
 };
