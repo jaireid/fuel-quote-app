d47f22f3afe46d7ec2d6b892154c0f998f821b12
Jasmaine Williams <jaiwilliamsreid@gmail.com>
2023-06-28 23:40:48 -0500

    Create user authentication controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import generateToken from "../utils/generateToken.js";
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Auth User" });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 // @desc    Register a new user
