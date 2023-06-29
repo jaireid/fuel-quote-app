b8013be789eb57af2a405d7d8dc2380a303c230f
Jasmaine Williams <jaiwilliamsreid@gmail.com>
2023-06-28 23:08:04 -0500

    Create register user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user/set token
 // @route   POST /api/users/auth
@@ -12,7 +13,37 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Register User" });
+    const { username, email, password, state, city } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+        state,
+        city
+    });
+
+
+    if(user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.username,
+            email: user.email,
+            state: user.state,
+            city: user.city
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
 });
 
 // @desc    Logout user
