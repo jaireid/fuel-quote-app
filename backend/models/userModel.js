c7f69c4dca92a577c8846bfd66c0eae121776ef0
Jasmaine Williams <jaiwilliamsreid@gmail.com>
2023-06-28 20:27:56 -0500

    Create quoteModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     email: {
         type: String,
