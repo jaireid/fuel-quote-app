2667c51928ff048f7012e93fc365df1ca0a2ddc3
Jasmaine Williams <jaiwilliamsreid@gmail.com>
2023-06-28 19:44:43 -0500

    Error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -18,5 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => res.send("Server is ready!"));
+
+// Handle errors
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
