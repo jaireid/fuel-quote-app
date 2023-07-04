5cc5a1d88b6fd8139291d77b6c2af53b5872c689
jaireid <jaiwilliamsreid@gmail.com>
2023-07-03 22:46:01 -0500

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,18 +11,18 @@ const Header = () => {
   return (
     <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
-          <span className="self-center text-2xl text-blue-600 font-semibold whitespace-nowrap">
+        <Link to="/">
+          <span className="flex items-center self-center text-2xl text-blue-600 font-semibold whitespace-nowrap">
             Fuel Quote
           </span>
-        </a>
+        </Link>
+
         <div className="flex md:order-2">
-          <a
-            href="/login"
-            className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm px-4 py-2 text-center mr-3 lg:mr-0"
-          >
-            Sign In <span aria-hidden="true">&rarr;</span>
-          </a>
+          <Link to="/login">
+            <button className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm px-4 py-2 text-center mr-3">
+              Sign In <span aria-hidden="true">&rarr;</span>
+            </button>
+          </Link>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
@@ -47,7 +48,7 @@ const Header = () => {
           </button>
         </div>
         <div
-          className={`items-center justify-between w-full lg:pr-20 md:flex md:w-auto md:order-1 ${
+          className={`items-center justify-between w-full mr-12 md:flex md:w-auto md:order-1 ${
             isMenuOpen ? "block" : "hidden"
           }`}
           id="navbar-sticky"
@@ -76,6 +77,14 @@ const Header = () => {
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:hover:text-blue-600 md:p-0"
               >
                 Profile
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:hover:text-blue-600 md:p-0"
+              >
+                Contact
               </a>
             </li>
           </ul>
