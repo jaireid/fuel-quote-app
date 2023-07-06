c231a36a7129c3feb3bf76f70f544f7854d8147a
jaireid <jaiwilliamsreid@gmail.com>
2023-07-05 20:44:20 -0500

    Set up quote API slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import { userApiSlice } from "./slices/userApiSlice";
+import { quoteApiSlice } from "./slices/quoteApiSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [userApiSlice.reducerPath]: userApiSlice.reducer,
+    [quoteApiSlice.reducerPath]: quoteApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true,
